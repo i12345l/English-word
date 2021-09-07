a6fc7b4c82c715f0febe507da4863963344f2520
i12345l <ys0102j1@naver.com>
2021-09-07 12:59:10 +0900

    파일 만들기

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-var word=[['1','2','3','4'],['5','6','7','8']]
+function start(){
 
-var now=0;
-
-function button_click(a){
-    alert("버튼"+a+"를 눌렀습니다.");
 }
 
-function pass(){
-    document.getElementById("a_1").value=word[now][0];
-    document.getElementById("a_2").value=word[now][1];
-    document.getElementById("a_3").value=word[now][2];
-    document.getElementById("a_4").value=word[now][3];
-    now++;
+function record(){
+    alert("구현x");
 }
